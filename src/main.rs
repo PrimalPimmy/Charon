3eeaa6052bdd95a1e010e3a22e661e30e409634d
PrimalPimmy <Prashant20.pm@gmail.com>
2025-09-02 18:05:21 +0530

    using nix instead of libc
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
-use std::fs::File;
-use std::io::{self};
-use std::os::unix::io::{AsRawFd, FromRawFd};
-use std::sync::atomic::{AtomicUsize, Ordering, AtomicU32};
-use nix::sys::memfd::{memfd_create, MemFdCreateFlag};
+use linux_futex::{AsFutex, Futex, Private};
+use nix::sys::memfd::{MemFdCreateFlag, memfd_create};
+use nix::sys::mman::{MapFlags, ProtFlags, mmap, munmap};
 use std::ffi::CString;
+use std::io::{self};
+use std::num::NonZero;
+use std::os::fd::AsRawFd;
+use std::ptr::NonNull;
+use std::sync::atomic::{AtomicU32, AtomicUsize, Ordering};
 
 const SHM_SIZE: usize = 4096;
-
-// Futex constants
-const FUTEX_WAIT: i32 = 0;
-const FUTEX_WAKE: i32 = 1;
-
-unsafe fn futex_wait(uaddr: *mut AtomicU32, val: u32, timeout: *const libc::timespec) -> i32 {
-    unsafe {
-        libc::syscall(
-            libc::SYS_futex,
-            uaddr,
-            FUTEX_WAIT,
-            val,
-            timeout,
-            0,
-            0,
-        ) as i32
-    }
-}
-
-unsafe fn futex_wake(uaddr: *mut AtomicU32, val: i32) -> i32 {
-    unsafe {
-        libc::syscall(
-            libc::SYS_futex,
-            uaddr,
-            FUTEX_WAKE,
-            val,
-            0,
-            0,
-            0,
-        ) as i32
-    }
-}
 
 #[repr(C)]
 struct ShmHeader {
@@ -46,41 +17,51 @@ struct ShmHeader {
     futex: AtomicU32,
 }
 
+use nix::unistd::close;
+use nix::unistd::ftruncate;
+
 struct ShmRingBuffer {
-    fd: File,
-    ptr: *mut u8,
+    ptr: NonNull<u8>,
+    fd: i32,
 }
 
 impl ShmRingBuffer {
     fn new(name: &str) -> io::Result<Self> {
         let c_name = CString::new(name).unwrap();
-        let fd = memfd_create(&c_name, MemFdCreateFlag::MFD_CLOEXEC).map_err(|e| io::Error::from_raw_os_error(e as i32))?;
-        let file = unsafe { File::from_raw_fd(fd.as_raw_fd()) };
-        file.set_len(SHM_SIZE as u64)?;
+        let fd = memfd_create(&c_name, MemFdCreateFlag::MFD_CLOEXEC)
+            .map_err(|e| io::Error::from_raw_os_error(e as i32))?;
+        ftruncate(&fd, SHM_SIZE as i64).map_err(|e| io::Error::from_raw_os_error(e as i32))?;
         let ptr = unsafe {
-            libc::mmap(
-                std::ptr::null_mut(),
-                SHM_SIZE,
-                libc::PROT_READ | libc::PROT_WRITE,
-                libc::MAP_SHARED,
-                file.as_raw_fd(),
+            mmap(
+                None,
+                NonZero::new(SHM_SIZE).unwrap(),
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                MapFlags::MAP_SHARED,
+                &fd,
                 0,
             )
-        } as *mut u8;
-        if ptr.is_null() {
-            return Err(io::Error::last_os_error());
         }
-        Ok(Self { fd: file, ptr })
+        .map_err(|e| io::Error::from_raw_os_error(e as i32))?;
+        Ok(Self {
+            ptr: ptr.cast(),
+            fd: fd.as_raw_fd(),
+        })
     }
 
     fn header(&self) -> &ShmHeader {
-        unsafe { &*(self.ptr as *const ShmHeader) }
+        unsafe { &*(self.ptr.as_ptr() as *const ShmHeader) }
+    }
+
+    fn futex(&self) -> &Futex<Private> {
+        self.header().futex.as_futex()
     }
 
     fn buffer(&self) -> &[u8] {
         unsafe {
             std::slice::from_raw_parts(
-                self.ptr.offset(std::mem::size_of::<ShmHeader>() as isize),
+                self.ptr
+                    .as_ptr()
+                    .offset(std::mem::size_of::<ShmHeader>() as isize),
                 SHM_SIZE - std::mem::size_of::<ShmHeader>(),
             )
         }
@@ -89,7 +70,9 @@ impl ShmRingBuffer {
     fn buffer_mut(&mut self) -> &mut [u8] {
         unsafe {
             std::slice::from_raw_parts_mut(
-                self.ptr.offset(std::mem::size_of::<ShmHeader>() as isize),
+                self.ptr
+                    .as_ptr()
+                    .offset(std::mem::size_of::<ShmHeader>() as isize),
                 SHM_SIZE - std::mem::size_of::<ShmHeader>(),
             )
         }
@@ -120,15 +103,17 @@ impl ShmRingBuffer {
                     buffer[head..head + bytes_to_write].copy_from_slice(data);
                 }
 
-                self.header().head.store((head + bytes_to_write) % buffer_len, Ordering::Release);
-                unsafe { futex_wake(&self.header().futex as *const _ as *mut AtomicU32, 1); }
+                self.header()
+                    .head
+                    .store((head + bytes_to_write) % buffer_len, Ordering::Release);
+                let _ = self.futex().wake(1);
                 return Ok(bytes_to_write);
             }
 
             // Buffer is full, wait
-            unsafe {
-                futex_wait(&self.header().futex as *const _ as *mut AtomicU32, 0, std::ptr::null());
-            }
+            let _ = self
+                .futex()
+                .wait(self.header().futex.load(Ordering::Relaxed));
         }
     }
 
@@ -141,9 +126,9 @@ impl ShmRingBuffer {
 
             if head == tail {
                 // Buffer is empty, wait
-                unsafe {
-                    futex_wait(&self.header().futex as *const _ as *mut AtomicU32, 0, std::ptr::null());
-                }
+                let _ = self
+                    .futex()
+                    .wait(self.header().futex.load(Ordering::Relaxed));
                 continue;
             }
 
@@ -159,13 +144,16 @@ impl ShmRingBuffer {
             if tail + bytes_to_read > buffer_len {
                 let first_chunk = buffer_len - tail;
                 data[..first_chunk].copy_from_slice(&buffer[tail..]);
-                data[first_chunk..bytes_to_read].copy_from_slice(&buffer[..bytes_to_read - first_chunk]);
+                data[first_chunk..bytes_to_read]
+                    .copy_from_slice(&buffer[..bytes_to_read - first_chunk]);
             } else {
                 data[..bytes_to_read].copy_from_slice(&buffer[tail..tail + bytes_to_read]);
             }
 
-            self.header().tail.store((tail + bytes_to_read) % buffer_len, Ordering::Release);
-            unsafe { futex_wake(&self.header().futex as *const _ as *mut AtomicU32, 1); }
+            self.header()
+                .tail
+                .store((tail + bytes_to_read) % buffer_len, Ordering::Release);
+            let _ = self.futex().wake(1);
             return Ok(bytes_to_read);
         }
     }
@@ -174,8 +162,9 @@ impl ShmRingBuffer {
 impl Drop for ShmRingBuffer {
     fn drop(&mut self) {
         unsafe {
-            libc::munmap(self.ptr as *mut libc::c_void, SHM_SIZE);
+            let _ = munmap(self.ptr.cast(), SHM_SIZE);
         }
+        let _ = close(self.fd);
     }
 }
 
@@ -193,7 +182,11 @@ fn main() {
 
     for (i, msg) in messages.iter().enumerate() {
         match shm.write(*msg) {
-            Ok(bytes_written) => println!("Wrote {} bytes: {}", bytes_written, String::from_utf8_lossy(*msg)),
+            Ok(bytes_written) => println!(
+                "Wrote {} bytes: {}",
+                bytes_written,
+                String::from_utf8_lossy(*msg)
+            ),
             Err(e) => eprintln!("Write error {}: {}", i, e),
         }
     }
@@ -202,9 +195,14 @@ fn main() {
         let mut read_buffer = [0u8; 1024];
         match shm.read(&mut read_buffer) {
             Ok(bytes_read) => {
-                println!("Read {} bytes: {}", bytes_read, String::from_utf8_lossy(&read_buffer[..bytes_read]));
+                println!(
+                    "Read {} bytes: {}",
+                    bytes_read,
+                    String::from_utf8_lossy(&read_buffer[..bytes_read])
+                );
             }
             Err(e) => eprintln!("Read error {}: {}", i, e),
         }
     }
 }
+
